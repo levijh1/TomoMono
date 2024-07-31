18d686738e787dc455b9988bc08b8f3440b13a6c
Levi Hancock <levijh1@gmail.com>
2024-07-31 09:53:10 -0700

    july 31 morning alignment run

--- pltwidget.py
+++ pltwidget.py
@@ -1,20 +1,22 @@
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
+import numpy as np
 
 
 
 
 def runwidget(m):
     fig, ax = plt.subplots(figsize=(9, 6))
-    ax.imshow(m[0])
-    ax.set_title("recon Normal")
+    ax.imshow(m[0], vmin=np.min(m), vmax=np.max(m), cmap='gray')
+    plt.title("Frame 0")
 
     plt.subplots_adjust(bottom=0.25)
     ax_slider = plt.axes([0.1,0.1, 0.8, 0.05], facecolor='teal')
 
     def update_line(indx):
         ax.clear()
-        ax.imshow(m[indx])
+        ax.imshow(m[indx], vmin=np.min(m), vmax=np.max(m), cmap='gray')
+        plt.title(f"Frame {indx}")
         plt.draw()
 
     slider = Slider(ax_slider, "Height (cross-section)", valmin=0, valmax=m.shape[0]-1, valinit=20, valstep = 1)
